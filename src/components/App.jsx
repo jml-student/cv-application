ea883037672efe9b66d83c393484d141d26d75f3
jml <student741@proton.me>
2024-10-08 12:13:19 +0200

    Create component files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,18 @@
-/*import { useState } from 'react'*/
+import { Information } from './Information.jsx'
+import { Experience } from './Experience.jsx'
+import { Practical } from './Practical.jsx'
 import './App.css'
 
 function App() {
-  //
+  return (
+    <>
+      <div className="App">
+        <Information />
+        <Experience />
+        <Practical />
+      </div>
+    </>
+  )
 }
 
 export default App
