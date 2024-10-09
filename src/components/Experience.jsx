0a524930a77ff74a820ee6660acedfe5fc8b1d0a
jml <student741@proton.me>
2024-10-09 21:04:29 +0200

    Add edit buttons

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -96,23 +96,34 @@ function Experience() {
           )
         })}
       </div>
-      <button
-        className="btn-experience"
-        onClick={() =>
-          setExperience([
-            ...experience,
-            { company: '', title: '', responsability: '', from: '', to: '' },
-          ])
-        }
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 24 24"
+      <div className="experience-buttons">
+        <button
+          className="btn-experience-add"
+          onClick={() =>
+            setExperience([
+              ...experience,
+              { company: '', title: '', responsability: '', from: '', to: '' },
+            ])
+          }
         >
-          <path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+          </svg>
+        </button>
+        <button className="btn-experience-edit">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" />
+          </svg>
+        </button>
+      </div>
     </div>
   )
 }
